6c464c0e31e960f65a9be4e0857b9978cc5820c0
Beedemo Admin <beedemo.sa@gmail.com>
2018-03-18 23:51:09 -0400

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,7 +1,7 @@
 //must be called from an agent that supports Docker
 def call(org, name, tag, dir, pushCredId) {
-    dockerImage = docker.build("$org/$name:$tag", "$dir")
+    sh "docker build -t $org/$name:$tag $dir"
     withDockerRegistry(registry: [credentialsId: "$pushCredId"]) { 
-        dockerImage.push()
+        sh "docker push $org/$name:$tag"
     }
 }
