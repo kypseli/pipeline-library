683e1c6da41b4eae3f3d4b4195fc7d30f7d32042
Beedemo Admin <beedemo.sa@gmail.com>
2018-03-18 21:10:46 -0400

    Update kubeDeploy.groovy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 
 def call(String imageName, String imageRepo, String imageTag, String environment) {
-  node('dind') {
+  node('docker') {
     container("kubectl") {
       sh "kubectl set image deployment/${name} ${name}-app=${imageRepo}/${name}:${imageTag}"
     }
