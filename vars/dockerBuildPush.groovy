96cf013618d356dd2758fc535be8136e023e97f3
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-05-16 15:58:10 -0400

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,6 +1,35 @@
 import org.kypseli.util.DockerBuildPush
 
 def call(name, tag) {
-    def dockerBuildPush = new DockerBuildPush(this)
-    dockerBuildPush.buildPush(name, tag)
+    def label = "kaniko-${UUID.randomUUID().toString()}"
+    podTemplate(name: 'kaniko', label: label, yaml: """
+     kind: Pod
+     metadata:
+       name: kaniko
+     spec:
+       containers:
+       - name: kaniko
+         image: csanchez/kaniko:jenkins # we need a patched version of kaniko for now
+         imagePullPolicy: Always
+         command:
+         - cat
+         tty: true
+         volumeMounts:
+           - name: jenkins-docker-cfg
+             mountPath: /root/.docker
+       volumes:
+         - name: jenkins-docker-cfg
+           secret:
+             secretName: jenkins-docker-cfg
+             items:
+             - key: .dockerconfigjson
+               path: config.json
+     """
+    ) {
+      node(label) {
+        steps.container('kaniko') {
+          steps.sh "/kaniko/executor -c . --destination=beedemo/${name}:${tag}"
+        }
+      }
+    }
 }
