2ad2ef3a869e1d5a74e07542ac595c50a23b7631
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-05-17 16:23:12 -0400

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -28,9 +28,9 @@ def call(String name, String tag, String target = ".", Closure body) {
       node(label) {
         body()
         container('kaniko') {
-          sh 'cp target/* /root'
-          sh 'ls -la /root'
-          sh "/kaniko/executor -f Dockerfile -c /root -d beedemo/${name}:${tag}"
+          sh 'cp target/* /kaniko'
+          sh 'ls -la /kaniko'
+          sh "/kaniko/executor -f Dockerfile -c /kaniko -d beedemo/${name}:${tag}"
         }
       }
     }
