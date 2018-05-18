9d03595838523ca8c7be60bbbe0225d4ab15b02f
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-05-18 11:45:28 -0400

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -8,16 +8,16 @@ def call(String name, String tag, String target = ".", Closure body) {
      spec:
        serviceAccountName: kaniko
        containers:
-       - name: kubectl
-         image: lachlanevenson/k8s-kubectl:v1.9.3
-         command:
-         - cat
-         tty: true
-       - name: kaniko
-         image: gcr.io/kaniko-project/executor:debug
-         command:
-         - /busybox/sh
-         tty: true
+         - name: kubectl
+           image: lachlanevenson/k8s-kubectl:v1.9.3
+           command:
+           - cat
+           tty: true
+         - name: kaniko
+           image: gcr.io/kaniko-project/executor:debug
+           command:
+           - /busybox/sh
+           tty: true
          volumeMounts:
           - name: podinfo
             mountPath: /etc/podinfo
