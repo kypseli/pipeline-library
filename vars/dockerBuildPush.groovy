89cb7c4f6ae4c2bd14e32baec5f8412b38c3107a
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-08-25 11:30:28 -0400

    Update dockerBuildPush.groovy
    Added ability to override dockerRepo - defaults to ecr and kypseli namespace/prefix

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,5 +1,5 @@
 // vars/dockerBuildPush.groovy
-def call(String name, String tag, String target = ".", String dockerFile="Dockerfile", Closure body) {
+def call(String name, String tag, String target = ".", String dockerFile="Dockerfile", String dockerRepo="946759952272.dkr.ecr.us-east-1.amazonaws.com/kypseli/", Closure body) {
     def label = "kaniko-${UUID.randomUUID().toString()}"
     podTemplate(name: 'kaniko', label: label, namespace: 'kaniko', yaml: """
      kind: Pod
@@ -23,12 +23,11 @@ def call(String name, String tag, String target = ".", String dockerFile="Docker
 """
     ) {
       node(label) {
-        def ecrPrefix = "946759952272.dkr.ecr.us-east-1.amazonaws.com/"
         container(name: 'kaniko', shell: '/busybox/sh') {
           body()
           withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
             sh """#!/busybox/sh
-              executor -f ${pwd()}/${dockerFile} -c ${pwd()} -d ${ecrPrefix}${name}:${tag} -d ${ecrPrefix}${name}:latest
+              executor -f ${pwd()}/${dockerFile} -c ${pwd()} -d ${dockerRepo}${name}:${tag} -d ${ecrPrefix}${name}:latest
             """
           }
         }
