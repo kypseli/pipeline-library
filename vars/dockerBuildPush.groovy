76df0a2458ddc552f63314670bb632dd3c394efc
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-08-27 14:09:02 -0400

    Update dockerBuildPush.groovy
    Updated to use yaml for pod template for resources

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,27 +1,8 @@
 // vars/dockerBuildPush.groovy
 def call(String name, String tag, String target = ".", String dockerFile="Dockerfile", String dockerRepo="946759952272.dkr.ecr.us-east-1.amazonaws.com/kypseli/", Closure body) {
     def label = "kaniko-${UUID.randomUUID().toString()}"
-    podTemplate(name: 'kaniko', label: label, namespace: 'kaniko', yaml: """
-     kind: Pod
-     metadata:
-       name: kaniko
-     spec:
-       containers:
-       - name: kaniko
-         image: gcr.io/kaniko-project/executor:debug-v0.3.0
-         command:
-         - /busybox/cat
-         tty: true
-         volumeMounts:
-           - name: docker-config
-             mountPath: /kaniko/.docker
-       volumes:
-         - name: docker-config
-           configMap:
-             name: docker-config
-       serviceAccountName: kaniko
-"""
-    ) {
+    def podYaml = libraryResource 'podtemplates/dockerBuildPusy.yml'
+    podTemplate(name: 'kaniko', label: label, namespace: 'kaniko', yaml: podYaml) {
       node(label) {
         container(name: 'kaniko', shell: '/busybox/sh') {
           body()
