8b45667494f3814984d5c979204193c2c9f45e10
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-08-28 17:37:02 -0400

    Removed inheritFrom
    Removed explicit `inheritFrom` as `default-jnlp` is set as the global default

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -2,7 +2,7 @@
 def call(String name, String tag, String target = ".", String dockerFile="Dockerfile", String dockerRepo="946759952272.dkr.ecr.us-east-1.amazonaws.com/kypseli/", Closure body) {
     def label = "kaniko-${UUID.randomUUID().toString()}"
     def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
-    podTemplate(name: 'kaniko', label: label, namespace: 'kaniko', inheritFrom: 'default-jnlp', yaml: podYaml) {
+    podTemplate(name: 'kaniko', label: label, namespace: 'kaniko',  yaml: podYaml) {
       node(label) {
         container(name: 'kaniko', shell: '/busybox/sh') {
           body()
