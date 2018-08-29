3ec17009000eefa8f6c35394dffba42274b2db37
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-08-29 15:31:26 -0400

    Update dockerFileDefaults.groovy
    Fixed brackets

--- vars/dockerFileDefaults.groovy
+++ vars/dockerFileDefaults.groovy
@@ -11,18 +11,19 @@ def call(body) {
     def label = "aws-cli-${UUID.randomUUID().toString()}"
     def podYaml = libraryResource 'podtemplates/awsCli.yml'
     podTemplate(name: 'aws-cli', label: label, yaml: podYaml) {
-    node(label) {
-      checkout scm
-      def d = [repo: repo, tag: tag, pushBranch: false]
-      def props = readProperties defaults: d, file: 'dockerBuildPublish.properties'
-      repoName = props['repo']
-      tag = props['tag']
-      pushBranch = props['pushBranch']
-      echo "push non master branch: $pushBranch"
-      stash name: 'everything', includes: '**'
-      stage 'Check Repository'
-      container('aws-cli') {
-        sh "aws ecr create-repository --repository-name kypselie/${repoName}"
+      node(label) {
+        checkout scm
+        def d = [repo: repo, tag: tag, pushBranch: false]
+        def props = readProperties defaults: d, file: 'dockerBuildPublish.properties'
+        repoName = props['repo']
+        tag = props['tag']
+        pushBranch = props['pushBranch']
+        echo "push non master branch: $pushBranch"
+        stash name: 'everything', includes: '**'
+        stage 'Check Repository'
+        container('aws-cli') {
+          sh "aws ecr create-repository --repository-name kypselie/${repoName}"
+        }
       }
     }
     
