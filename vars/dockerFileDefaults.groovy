faea7b42a8f3075f3f32b3c51105fe0b68d54a89
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-08-29 15:30:48 -0400

    Update dockerFileDefaults.groovy
    Added aws cli to create repository

--- vars/dockerFileDefaults.groovy
+++ vars/dockerFileDefaults.groovy
@@ -8,7 +8,10 @@ def call(body) {
     repo = tokens[tokens.size()-2]
     tag = tokens[tokens.size()-1]
     
-    node('default-jnlp') {
+    def label = "aws-cli-${UUID.randomUUID().toString()}"
+    def podYaml = libraryResource 'podtemplates/awsCli.yml'
+    podTemplate(name: 'aws-cli', label: label, yaml: podYaml) {
+    node(label) {
       checkout scm
       def d = [repo: repo, tag: tag, pushBranch: false]
       def props = readProperties defaults: d, file: 'dockerBuildPublish.properties'
@@ -17,6 +20,10 @@ def call(body) {
       pushBranch = props['pushBranch']
       echo "push non master branch: $pushBranch"
       stash name: 'everything', includes: '**'
+      stage 'Check Repository'
+      container('aws-cli') {
+        sh "aws ecr create-repository --repository-name kypselie/${repoName}"
+      }
     }
     
     if(env.BRANCH_NAME=="master" || pushBranch) {
