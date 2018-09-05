b616e6be5b136d7195922e5a7e8c941729881baa
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-09-05 16:43:21 -0400

    Update dockerBuildPush.groovy
    removed jnlp test container

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -4,9 +4,6 @@ def call(String name, String tag, String target = ".", String dockerFile="Docker
     def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
     podTemplate(name: 'kaniko', label: label, namespace: 'kaniko',  yaml: podYaml) {
       node(label) {
-        container('k8s-jnlp') {
-          sh 'ls -la /home/jenkins'
-        }
         container(name: 'kaniko', shell: '/busybox/sh') {
           body()
           withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
