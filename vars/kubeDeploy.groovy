6835ec129a5391fed7fe50c87ec883f7233bdd0a
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-09-09 10:59:08 -0400

    Update kubeDeploy.groovy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,4 +1,7 @@
 def call(imageName, imageRepo, imageTag, environment) {
+    def label = "kubectl"
+    def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
+    podTemplate(name: 'kubectl', label: label, namespace: 'agents',  yaml: podYaml) {
     container("kubectl") {
       sh "kubectl set image deployment/${name} ${name}-app=${imageRepo}/${name}:${imageTag}"
     }
