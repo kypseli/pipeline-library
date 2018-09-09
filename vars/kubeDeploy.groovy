4db4b59cb7710ae38d0a81359474eaacce0ff99c
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-09-09 11:10:05 -0400

    Update kubeDeploy.groovy
    Added `kaniko` namespace

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,7 +1,7 @@
 def call(imageName, imageRepo, imageTag, environment='staging') {
     def label = "kubectl"
     def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
-    podTemplate(name: 'kubectl', label: label,  yaml: podYaml) {
+    podTemplate(name: 'kubectl', label: label, namespace: 'kaniko',  yaml: podYaml) {
       node(label) {
         container("kubectl") {
           sh "kubectl set image deployment/${name} ${name}-app=${imageRepo}/${name}:${imageTag}"
