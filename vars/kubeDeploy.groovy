57b52fe6c1399ef0d69b1f6d43793ae40985dbf3
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-09-10 20:38:47 -0400

    Update kubeDeploy.groovy
    Updated to work with file from build repo

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,10 +1,11 @@
-def call(imageName, imageRepo, imageTag, environment='staging') {
+def call(imageName, imageRepo, imageTag, deployYmlPath, environment='staging') {
     def label = "kubectl"
     def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
     podTemplate(name: 'kubectl', label: label, namespace: 'cb-deploy',  yaml: podYaml) {
       node(label) {
         container("kubectl") {
-          sh "kubectl set image deployment/${name} ${name}-app=${imageRepo}/${name}:${imageTag}"
+          sh("sed -i.bak 's#REPLACE_IMAGE_TAG#946759952272.dkr.ecr.us-east-1.amazonaws.com/${imageRepo}/${imageName}:${imageTag}#' ./${deployYmlPath}")
+          sh "kubectl --namespace=cb-deploy apply -f ${deployYmlPath}"
         }
       }
     }
