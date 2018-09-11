66122dfcd53ba7c20c5248883974b862b3104f5d
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-09-10 21:06:40 -0400

    Update kubeDeploy.groovy
    Attempt to fix deployYamlPath

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,11 +1,11 @@
-def call(imageName, imageRepo, imageTag, deployYmlPath, environment='staging') {
+def call(imageName, imageRepo, imageTag, deployYamlPath, environment='staging') {
     def label = "kubectl"
     def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
     podTemplate(name: 'kubectl', label: label, namespace: 'cb-deploy',  yaml: podYaml) {
       node(label) {
         container("kubectl") {
-          sh("sed -i.bak 's#REPLACE_IMAGE_TAG#946759952272.dkr.ecr.us-east-1.amazonaws.com/${imageRepo}/${imageName}:${imageTag}#' ./${deployYmlPath}")
-          sh "kubectl --namespace=cb-deploy apply -f ${deployYmlPath}"
+          sh("sed -i.bak 's#REPLACE_IMAGE_TAG#946759952272.dkr.ecr.us-east-1.amazonaws.com/${imageRepo}/${imageName}:${imageTag}#' ${deployYamlPath}")
+          sh "kubectl --namespace=cb-deploy apply -f ${deployYamlPath}"
         }
       }
     }
