5e1e21d43444f15f61e442ff9932cc34ac16f9bd
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2018-09-10 21:10:57 -0400

    Update kubeDeploy.groovy
    Added unstash

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -4,6 +4,7 @@ def call(imageName, imageRepo, imageTag, deployYamlPath, environment='staging') 
     podTemplate(name: 'kubectl', label: label, namespace: 'cb-deploy',  yaml: podYaml) {
       node(label) {
         container("kubectl") {
+          unstash 'deploy'
           sh("sed -i.bak 's#REPLACE_IMAGE_TAG#946759952272.dkr.ecr.us-east-1.amazonaws.com/${imageRepo}/${imageName}:${imageTag}#' ${deployYamlPath}")
           sh "kubectl --namespace=cb-deploy apply -f ${deployYamlPath}"
         }
