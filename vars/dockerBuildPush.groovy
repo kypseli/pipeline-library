27189cea625532270a330788ef01ac6a3a6f6121
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2019-01-11 05:23:00 -0500

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -2,7 +2,7 @@
 def call(String name, String tag, String target = ".", String dockerFile="Dockerfile", String dockerRepo="946759952272.dkr.ecr.us-east-1.amazonaws.com/kypseli/", Closure body) {
     def label = "kaniko"
     def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
-    podTemplate(name: 'kaniko', label: label, yaml: podYaml, inheritFrom: 'kaniko-base') {
+    podTemplate(name: 'kaniko', label: label, yaml: podYaml, namespace: 'kaniko') {
       node(label) {
         container(name: 'kaniko', shell: '/busybox/sh') {
           body()
