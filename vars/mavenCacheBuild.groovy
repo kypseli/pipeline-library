71a34a597242473446299ac81f242b1da1e5f861
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2019-02-05 07:57:06 -0500

    Update mavenCacheBuild.groovy

--- vars/mavenCacheBuild.groovy
+++ vars/mavenCacheBuild.groovy
@@ -1,11 +1,23 @@
 def call(String name, String team='default') {
   def label = "mvn-cache"
   def podYaml = libraryResource 'podtemplates/mvnCache.yml'
-  podTemplate(name: 'mvn-cache', label: label, namespace: 'jenkins-agents',  yaml: podYaml) {
-    node(label) {
-      checkout scm
-      container("maven") {
-        sh "mvn dependency:resolve"
+  
+  try {
+    podTemplate(name: 'mvn', label: 'mvn', namespace: 'jenkins-agents', yaml: podYaml) {
+      node('mvn') {
+        checkout scm
+        container("maven") {
+          sh "mvn -o package"
+        }
+      }
+    }
+  } catch(error) {
+    podTemplate(name: 'mvn-cache', label: label, namespace: 'jenkins-agents', idleMinutes: 480, yaml: podYaml) {
+      node(label) {
+        checkout scm
+        container("maven") {
+          sh "mvn package"
+        }
       }
     }
   }
