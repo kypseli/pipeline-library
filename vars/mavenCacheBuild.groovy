8af327c9a0952c19186e407defd7a007ae970f60
Beedemo Ops <37405609+beedemo-ops@users.noreply.github.com>
2019-02-05 06:45:00 -0500

    Update mavenCacheBuild.groovy

--- vars/mavenCacheBuild.groovy
+++ vars/mavenCacheBuild.groovy
@@ -1,17 +1,8 @@
 def call(String name, String team='default') {
-  def label = "kubectl-mvn"
-  def podYaml = libraryResource 'podtemplates/kubectlMaven.yml'
-  def pvcYaml = libraryResource 'podtemplates/mavenCachePVC.yml'
-  podTemplate(name: 'kubectl-mvn', label: label, namespace: 'jenkins-agents',  yaml: podYaml) {
+  def label = "mvn-cache"
+  def podYaml = libraryResource 'podtemplates/mvnCache.yml'
+  podTemplate(name: 'mvn-cache', label: label, namespace: 'jenkins-agents',  yaml: podYaml) {
     node(label) {
-      container("kubectl") {
-        try {
-          sh("sed -i.bak 's#REPLACE_NAME#${team}#' ${pvcYaml}")
-          sh "kubectl --namespace=jenkins-agents apply -f ${pvcYaml}"
-        } catch(error) {
-          echo 'boom'
-        }
-      }
       container("maven") {
         sh "mvn dependency:resolve"
       }
